6e8ec60f664033b5767b985354a48f10b83e11dd
Jason <nocvi111@gmail.com>
2022-10-02 23:33:40 +0800

    chore: testing static file

--- routes/static.go
+++ routes/static.go
@@ -5,5 +5,5 @@ import (
 )
 
 func RegisteStaticContentRoutes(r *gin.Engine) {
-	r.StaticFile("/", "/react_app/actions-runner/image_proxy/Image-Proxy/Image-Proxy/build/index.html")
+	r.StaticFile("/", "./testing.html")
 }
