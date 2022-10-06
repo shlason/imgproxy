0b921a0855322e5023e2aa4bff9fedc766da5c81
Jason <nocvi111@gmail.com>
2022-10-06 17:22:22 +0800

    feat: FE static path

--- routes/static.go
+++ routes/static.go
@@ -1,9 +1,12 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/shlason/imgproxy/configs"
 )
 
 func RegisteStaticContentRoutes(r *gin.Engine) {
-	r.StaticFile("/", "./testing.html")
+	r.StaticFile("/", fmt.Sprintf("%s/index.html", configs.Server.FeWrokDir))
 }
