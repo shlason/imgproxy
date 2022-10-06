16168504cbc668278b10789097f47c5144911d9e
Jason <nocvi111@gmail.com>
2022-10-06 17:28:03 +0800

    feat: static root path

--- routes/static.go
+++ routes/static.go
@@ -8,5 +8,6 @@ import (
 )
 
 func RegisteStaticContentRoutes(r *gin.Engine) {
+	r.Static("/static", fmt.Sprintf("%s/static", configs.Server.FeWrokDir))
 	r.StaticFile("/", fmt.Sprintf("%s/index.html", configs.Server.FeWrokDir))
 }
